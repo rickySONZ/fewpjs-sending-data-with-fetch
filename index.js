ab7abc307cc775f750c19bcae6d599c22de82a1b
Ryan Erricson <ryanerricson@gmail.com>
2021-05-06 12:58:47 -0400

    commit tree 1afe297ff291e780881ad1e7c6895a765897d9aa

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+function submitData() {
+    let formData = {
+        name: "Steve",
+        email: "steve@steve.com"
+    };
+    
+    let configObj = {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+    
+    return fetch("http://localhost:3000/users", configObj)
+     .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        addMessageToPage(object.id);
+      })
+      .catch(function(error) {
+          alert("Unauthorized Access");
+          addMessageToPage(error)
+      })
+}
+
+
+function addMessageToPage(object){
+    let newObj = document.createElement('p')
+    newObj.innerHTML =  object
+    document.body.appendChild(newObj)
+}
